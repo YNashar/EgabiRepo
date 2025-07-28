53a2787a7b5843210b062c1c0fb2eb7c72f483c2
YNashar <youssef.alnashar1207@gmail.com>
2025-07-28 18:12:10 +0300

    Removed incorrect Backend folder

--- Backend/src/main/java/com/egabi/Repository/EnrollmentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.egabi.Repository;
-
-import com.egabi.Main.Enrollment;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EnrollmentRepository extends JpaRepository<Enrollment, Integer> {
-}
--- Backend/src/main/java/com/egabi/Repository/FacultyRepository.java
+++
@@ -1,8 +0,0 @@
-package com.egabi.Repository;
-
-import com.egabi.Main.Faculty;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FacultyRepository extends JpaRepository<Faculty, Integer> {
-    Faculty findByFacultyName(String facultyName);
-}
