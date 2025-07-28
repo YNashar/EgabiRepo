a20cd6c4dcdba374bd968b60f8a3fd3c50022d6f
YNashar <youssef.alnashar1207@gmail.com>
2025-07-28 17:55:27 +0300

    Replaced old Backend with new updated version

--- Backend/src/main/java/com/egabi/Repository/EnrollmentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.egabi.Repository;
-
-import com.egabi.Enrollment;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EnrollmentRepository extends JpaRepository<Enrollment, Integer> {
-}
--- Backend/src/main/java/com/egabi/Repository/GradeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.egabi.Repository;
-
-import com.egabi.Grade;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface GradeRepository extends JpaRepository<Grade, Integer> {
-}
